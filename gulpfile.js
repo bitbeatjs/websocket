7b57788b7b8cb02f2a1adcdd09a229b2d9883624
Oliver Freudrich <info@oliverfreudrich.com>
2020-09-03 14:07:04 +0200

    * fixed build job + updated package

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,35 @@
 const gulp = require('gulp');
+const changedInPlace = require('gulp-changed-in-place');
 const ts = require('gulp-typescript');
 const terser = require('gulp-terser');
 const merge = require('merge-stream');
 const eslint = require('gulp-eslint');
 
+const tsProject = ts.createProject('tsconfig.json');
 const compileTypeScript = () => {
-  const tsProject = ts.createProject('tsconfig.json');
   const tsResult = tsProject.src()
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError())
-    .pipe(tsProject());
+      .pipe(changedInPlace())
+      .pipe(eslint())
+      .pipe(eslint.format())
+      .pipe(eslint.failAfterError())
+      .pipe(tsProject());
 
   return merge(
-    tsResult.dts,
-    tsResult.js
-      .pipe(terser({
-        keep_classnames: true,
-        keep_fnames: true,
-        mangle: true
-      }))
-    )
-    .pipe(gulp.dest(__dirname));
+      tsResult.dts,
+      tsResult.js
+          .pipe(terser({
+            keep_classnames: true,
+            keep_fnames: true,
+            mangle: true
+          }))
+  )
+      .pipe(gulp.dest(__dirname));
 };
 
 gulp.task('default', compileTypeScript);
 gulp.task('watch', async () => {
   compileTypeScript();
   const files = await new Promise((res, rej) => {
-    const tsProject = ts.createProject('tsconfig.json');
     const stream = tsProject.src();
     const data = [];
 
