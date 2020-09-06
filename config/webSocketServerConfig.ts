be8bd79fdf832afd2f95420f18cdd2d026de91b9
Oliver Freudrich <info@oliverfreudrich.com>
2020-09-06 18:34:25 +0200

    - removed comments

--- config/webSocketServerConfig.ts
+++ config/webSocketServerConfig.ts
@@ -6,7 +6,6 @@ export default class WebSocketServerConfig extends Configuration {
         port: 5000,
         perMessageDeflate: {
             zlibDeflateOptions: {
-                // See zlib defaults.
                 chunkSize: 1024,
                 memLevel: 7,
                 level: 3,
@@ -14,14 +13,11 @@ export default class WebSocketServerConfig extends Configuration {
             zlibInflateOptions: {
                 chunkSize: 10 * 1024,
             },
-            // Other options settable:
-            clientNoContextTakeover: true, // Defaults to negotiated value.
-            serverNoContextTakeover: true, // Defaults to negotiated value.
-            serverMaxWindowBits: 10, // Defaults to negotiated value.
-            // Below options specified as default values.
-            concurrencyLimit: 10, // Limits zlib concurrency for perf.
-            threshold: 1024, // Size (in bytes) below which messages
-            // should not be compressed.
+            clientNoContextTakeover: true,
+            serverNoContextTakeover: true,
+            serverMaxWindowBits: 10,
+            concurrencyLimit: 10,
+            threshold: 1024,
         },
     };
 
